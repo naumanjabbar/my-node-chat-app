33d35372813bf3a7fba00e642cbd16479c381410
Nauman jabbar <naumanjabbar260@gmail.com>
2018-09-08 00:31:16 +0500

    add css for chat

--- public/js/index.js
+++ public/js/index.js
@@ -1,59 +1,60 @@
-var socket=io()
+var socket = io();
 
-  socket.on("connect",function(){
-    console.log("connected to server") 
-  })
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
 
-  socket.on("disconnect",function(){
-    console.log("disconnected from server")
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
 
-  })
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
 
-  socket.on("newMessage",function(message){
-    console.log("new message",message)
-    var li=jQuery("<li></li>")
-    li.text(`${message.from}: ${message.text}`)
+  jQuery('#messages').append(li);
+});
 
-    jQuery("#messages").append(li)
-  })
+socket.on('newLocationMessage', function (message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
 
-  socket.on("newLocationMessage",function(message){
-    var li =jQuery("<li></li>")
-    var a=jQuery('<a target="_blank">My current location</a>')
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
 
-    li.text(`${message.from}:`)
-    a.attr("href",message.url)
-    li.append(a)
-    jQuery("#messages").append(li)
-  })
-  socket.on("newMessage",function(message){
-    console.log("new message",message)
-  })
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
 
-  jQuery("#message-form").on("submit",function(e){
-    e.preventDefault()
+  var messageTextbox = jQuery('[name=message]');
 
-    socket.emit("createMessage",{
-      from:"user",
-      text:jQuery("[name=message]").val()
-    },function(){
-      
-    })
-  })
+  socket.emit('createMessage', {
+    from: 'User',
+    text: messageTextbox.val()
+  }, function () {
+    messageTextbox.val('')
+  });
+});
 
-var locationButton = jQuery("#send-location")
-locationButton.on("click",function(){
-  if(!navigator.geolocation){
-    return alert("geolocation not supported by your browser")
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
   }
 
-  navigator.geolocation.getCurrentPosition(function(position){
-    socket.emit("createLocationMessage",{
-      latitude:position.coords.latitude,
-      longitude:position.coords.longitude
-    })
-  },function(){
-    alert("unable to fetch location")
-  })
-})
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
 
+  navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
+    alert('Unable to fetch location.');
+  });
+});
