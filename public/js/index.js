836c76f7765cd17e3ac8733a14eb27a4d30a9ac0
Nauman jabbar <naumanjabbar260@gmail.com>
2018-09-07 23:36:10 +0500

    add geolocation support with api

--- public/js/index.js
+++ public/js/index.js
@@ -17,12 +17,18 @@ var socket=io()
     jQuery("#messages").append(li)
   })
 
+  socket.on("newLocationMessage",function(message){
+    var li =jQuery("<li></li>")
+    var a=jQuery('<a target="_blank">My current location</a>')
 
+    li.text(`${message.from}:`)
+    a.attr("href",message.url)
+    li.append(a)
+    jQuery("#messages").append(li)
+  })
   socket.on("newMessage",function(message){
     console.log("new message",message)
   })
-
- 
 
   jQuery("#message-form").on("submit",function(e){
     e.preventDefault()
@@ -34,3 +40,20 @@ var socket=io()
       
     })
   })
+
+var locationButton = jQuery("#send-location")
+locationButton.on("click",function(){
+  if(!navigator.geolocation){
+    return alert("geolocation not supported by your browser")
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position){
+    socket.emit("createLocationMessage",{
+      latitude:position.coords.latitude,
+      longitude:position.coords.longitude
+    })
+  },function(){
+    alert("unable to fetch location")
+  })
+})
+
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -19,15 +19,14 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message,callback) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
-    io.emit('newMessage', generateMessage(message.from, message.text))
-    callback("this is from the server")
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.');
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
